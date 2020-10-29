f5adef97d59d8f586b7f33fb1fda6c6084b56b8d
Santa dd <hethere97@gmail.com>
2020-10-29 09:58:24 +0000

    Templates modified

--- trial/forms.py
+++ trial/forms.py
@@ -4,27 +4,27 @@ from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, Length, Email, NumberRange
 
 
-#Create a Defect Form
-class DefectForm(FlaskForm):
-    report = RadioField('label', choices=[('report', 'report /'), ('complain', 'complain')])
+#Create a Defect Report Form
+class DefectReportForm(FlaskForm):
+    report = RadioField('label', validators=[DataRequired()], choices=[('report', 'report /'), ('complain', 'complain')])
     road_desc = StringField('Road', validators=[DataRequired(), Length(min=2, max=50)])
     dist_desc = StringField('District', validators=[DataRequired(), Length(min=2, max=50)])
     reg_desc = StringField('Region', validators=[DataRequired(), Length(min=2, max=50)])
     direction = StringField('Direction', validators=[DataRequired(), Length(min=2, max=50)])
-    details = TextAreaField('', validators=[DataRequired()])
+    details = TextAreaField('', validators=[DataRequired(), Length(min=2)])
     name_desc = StringField('Name:', validators=[DataRequired(), Length(min=2, max=50)])
-    mobile_no = IntegerField('Telephone No: ', validators=[DataRequired()])
+    mobile_no = IntegerField('Telephone No: ', validators=[DataRequired(message="Please enter a valid mobile number")])
     email = StringField('Email:', validators=[DataRequired(), Email()])
     date = DateField('Date:', validators=[DataRequired()], format='%Y-%m-%d')
     submit = SubmitField('Submit')
 
 
-class justForm(FlaskForm):
+class LeaveForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=2, max=50)])
     rank = StringField('Rank', validators=[DataRequired(), Length(min=2, max=50)])
     section = StringField('Section', validators=[DataRequired(), Length(min=2, max=50)])
-    date_app = DateField('Date', format='%Y-%m-%d')
-    tele_no = IntegerField('Telephone No', validators=[DataRequired(message="Please enter a valid number")])
+    date_app = DateField('Date', validators=[DataRequired()], format='%Y-%m-%d')
+    tele_no = IntegerField('Telephone No', validators=[DataRequired(message="Please enter a valid mobile number")])
     leave_cat = SelectField('Leave Category', validators=[DataRequired(message="Please Choose a Category")], choices=[('', 'Please Select an Option'),
                             ('Annual/Part Leave', 'Annual/Part Leave'),('Casual Leave', 'Casual Leave'),
                             ('Maternity Leave', 'Maternity Leave'),('Sick Leave', 'Sick Leave'),
@@ -33,9 +33,9 @@ class justForm(FlaskForm):
     no_of_days = IntegerField('No. of Days/Months ', validators=[DataRequired(message="Please enter a valid number")])
     start_date = DateField('From:', format='%Y-%m-%d')
     end_date = DateField('To:', format='%Y-%m-%d')
-    supp_info = TextAreaField('Supplementary Information', validators=[DataRequired()])
+    supp_info = TextAreaField('Supplementary Information', validators=[DataRequired(), Length(min=2)])
     address = StringField('Address', validators=[DataRequired(), Length(min=2, max=50)])
-    mobile_no = IntegerField('Mobile No', validators=[DataRequired(message="Please enter a valid number")])
+    mobile_no = IntegerField('Mobile No', validators=[DataRequired(message="Please enter a valid mobile number")])
     email = StringField('Email', validators=[DataRequired(), Email()])
     days_proceed = IntegerField('Days to proceed', validators=[DataRequired(message="Please enter a valid number")])
     effec_date = DateField('Effective Date of Leave', format='%Y-%m-%d')
@@ -43,27 +43,3 @@ class justForm(FlaskForm):
     outs_days = IntegerField('Outstanding Leave Day(s)', validators=[DataRequired(message="Please enter a valid number")])
     submit = SubmitField('Submit & Preview')
 
-#Create Leave Form
-class LeaveForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=50)])
-    rank = StringField('Rank', validators=[DataRequired(), Length(min=2, max=50)])
-    section = StringField('section', validators=[DataRequired(), Length(min=2, max=50)])
-    tel_no = IntegerField('Telephone No', validators=[DataRequired()])
-    no_of_days = IntegerField('No. of Days/Months ', validators=[DataRequired()])
-    start_date = DateField('From:',  format='%Y-%m-%d')
-    end_date = DateField('To:',  format='%Y-%m-%d')
-    supp_info = TextAreaField('Supplementary Information', validators=[DataRequired()])
-    address = StringField('Address', validators=[DataRequired(), Length(min=2, max=50)])
-    mobile_no = IntegerField('Mobile No', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    days_proceed = IntegerField('Days to proceed', validators=[DataRequired()])
-    effec_date = DateField('Effective Date of Leave',  format='%Y-%m-%d')
-    resump_date = DateField('Date of Resumption',  format='%Y-%m-%d')
-    outs_days = IntegerField('Outstanding Leave Day(s)', validators=[DataRequired()])
-    leave_cat = SelectField('Leave Category', 
-                            choices=[('', 'Please Select an Option'), ('Annual/Part Leave', 'Annual/Part Leave'),
-                            ('Casual Leave', 'Casual Leave'), ('Maternity Leave', 'Maternity Leave'),
-                            ('Sick Leave', 'Sick Leave'), ('Compassionate Leave', 'Compassionate Leave'),
-                            ('Study Leave', 'Study Leave'), ('Examination Leave', 'Examination Leave'),
-                            ('Resettlement Leave', 'Resettlement Leave')])
-    submit = SubmitField('Submit & Preview')
--- trial/routes.py
+++ trial/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, url_for, flash, redirect, request
-from trial.forms import DefectForm, LeaveForm, justForm
+from flask import render_template, url_for, flash, redirect
+from trial.forms import DefectReportForm, LeaveForm
 from trial.models import Leave
 from trial import app, db
 
@@ -18,40 +18,34 @@ def blog():
 def division():
     return render_template('divisions.html', title='Division')
 
-#Create a route for divisions
+#Create a route for basic layout
 @app.route('/basic')
 def basic():
     return render_template('basic_temp.html', title='Basic')
 
-#Create a route for divisions
-@app.route('/defect')
+#Create a route for defect form
+@app.route('/defect', methods=['GET', 'POST'])
 def defect():
-    form = DefectForm()
-    return render_template('defect.html', title='Road Defects Report', form=form)
-
-    
-@app.route('/just', methods=['GET', 'POST'])
-def just():
-    form = justForm()
+    form = DefectReportForm()
     if form.validate_on_submit():
-        leave = Leave(name=form.name.data, rank=form.rank.data, section=form.section.data, date_app=form.date_app.data,
+        return redirect(url_for('home'))
+    return render_template('defect_rep.html', title='Road Defects Report', form=form)
+
+
+#Create a route for just form layout    
+@app.route('/leave', methods=['GET', 'POST'])
+def leave():
+    form = LeaveForm()
+    if form.validate_on_submit():
+        le_ave = Leave(name=form.name.data, rank=form.rank.data, section=form.section.data, date_app=form.date_app.data,
                     tele_no=form.tele_no.data, leave_cat=form.leave_cat.data, no_of_days=form.no_of_days.data, 
                     start_date=form.start_date.data, end_date=form.end_date.data, supp_info=form.supp_info.data,
                     address=form.address.data, mobile_no=form.mobile_no.data, email=form.email.data, 
                     days_proceed=form.days_proceed.data, effec_date=form.effec_date.data, resump_date=form.resump_date.data,
                     outs_days=form.outs_days.data)
-        db.session.add(leave)
+        db.session.add(le_ave)
         db.session.commit()
         flash(f"Leave form submitted successfully", 'success')
         return redirect(url_for('just'))
-    return render_template('just.html', form=form)
-
-
-
-#Create a route for leave
-@app.route('/leave', methods=['GET', 'POST'])
-def leave():
-    form = LeaveForm()
-    if form.validate_on_submit():
-        return render_template('home.html')
     return render_template('leave_form.html', title='Leave Form Report', form=form)
+
