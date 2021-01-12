ddc9bab904db5752624f26f52e5cc15d0933dca1
Santa dd <hethere97@gmail.com>
2021-01-12 05:04:23 +0000

    ...

--- trial/admin/routes.py
+++ trial/admin/routes.py
@@ -23,8 +23,7 @@ def dashboard():
 
 
 @admin.route('/register', methods=['GET', 'POST'])
-@login_required
-@admin_required
+
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
