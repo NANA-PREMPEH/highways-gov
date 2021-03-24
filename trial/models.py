c7da9639843878aa91b0be4edb172a05f897b45b
Santadd <hethere97@gmail.com>
2021-03-24 11:57:57 +0000

    Configuration

--- trial/models.py
+++ trial/models.py
@@ -49,7 +49,7 @@ class SearchableMixin(object):
 
 db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
 db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
-
+ 
 
 
 
