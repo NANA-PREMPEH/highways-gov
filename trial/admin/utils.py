2ace3cc943906286a2fd80162b0dc265eeed80a4
Santadd <hethere97@gmail.com>
2021-12-07 13:10:32 +0000

    Blob storage feature modified

--- trial/admin/utils.py
+++ trial/admin/utils.py
@@ -1,8 +1,10 @@
 import os
 from flask import current_app
 import secrets
-from PIL import Image
-import boto3
+
+
+from trial import azure_storage
+from azure.storage.blob import ContentSettings
 
 
 #Takes picture data as an argument
@@ -16,8 +18,8 @@ def save_photo(photo):
     photo_path = os.path.join(current_app.root_path, 'static/blog_images', photo_name)
 
     photo.save(photo_path)
-    s3 = boto3.resource('s3', region_name='us-east-1')
-    s3.Bucket('santa-gha').upload_file(photo_path, 'static/blog_images/'+photo_name, ExtraArgs={'ACL':'public-read'})
+    
+    azure_storage.block_blob_service.create_blob_from_path(container_name='static/assets/static/blog_images', blob_name=photo_name, file_path=photo_path, content_settings=ContentSettings(content_type='image'))
 
     return photo_name 
 
@@ -35,8 +37,9 @@ def save_picture(form_picture):
     form_picture.save(picture_path)
 
 
-    s3 = boto3.resource('s3', region_name='us-east-1')
-    s3.Bucket('santa-gha').upload_file(picture_path, 'static/profile_pics/'+picture_fn, ExtraArgs={'ACL':'public-read'})
+    
+    #Store in azure blob
+    azure_storage.block_blob_service.create_blob_from_path(container_name='static/assets/static/profile_pics', blob_name=picture_fn, file_path=picture_path, content_settings=ContentSettings(content_type='image'))
 
     return picture_fn
 
@@ -52,8 +55,9 @@ def save_proj_image(form_picture):
     form_picture.save(picture_path)
 
 
-    s3 = boto3.resource('s3', region_name='us-east-1')
-    s3.Bucket('santa-gha').upload_file(picture_path, 'static/completed_proj/'+picture_fn, ExtraArgs={'ACL':'public-read'})
+    
+    #Store in azure blob
+    azure_storage.block_blob_service.create_blob_from_path(container_name='static/assets/static/completed_proj', blob_name=picture_fn, file_path=picture_path, content_settings=ContentSettings(content_type='image'))
 
     return picture_fn
 
@@ -69,8 +73,8 @@ def save_gallery_image(form_picture):
     form_picture.save(picture_path)
 
 
-    s3 = boto3.resource('s3', region_name='us-east-1')
-    s3.Bucket('santa-gha').upload_file(picture_path, 'static/gallery/'+picture_fn, ExtraArgs={'ACL':'public-read'})
-
+    #Store in azure blob
+    azure_storage.block_blob_service.create_blob_from_path(container_name='static/assets/static/gallery', blob_name=picture_fn, file_path=picture_path, content_settings=ContentSettings(content_type='image'))
+    
     return picture_fn
 
